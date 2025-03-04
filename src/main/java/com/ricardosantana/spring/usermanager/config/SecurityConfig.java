06c71bc6484d77893bb9ee2ddfe6d1e99fab8667
ricardosantanadev4 <ricardosantanadev4@gmail.com>
2025-03-03 23:55:09 -0300

    refactor: URL alterada.

--- src/main/java/com/ricardosantana/spring/usermanager/config/SecurityConfig.java
+++ src/main/java/com/ricardosantana/spring/usermanager/config/SecurityConfig.java
@@ -181,8 +181,7 @@ public class SecurityConfig {
         config.setAllowedOrigins(List.of(
                 "http://localhost:4200",
                 "https://user-manager-angular.vercel.app",
-                "https://user-manager-spring-production.up.railway.app",
-                "https://sring-boot-admin.railway.internal"));
+                "https://user-manager-spring-production.up.railway.app"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*")); // Permite todos os headers na requisição
         config.setExposedHeaders(List.of(HttpHeaders.AUTHORIZATION)); // Expondo o header Authorization
